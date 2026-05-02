1b766eaefa55e9a08c92334cc2570e36dcf13e29
SophiaShawnXanderVlad <sophialanidizon15@gmail.com>
2026-05-02 15:27:26 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('Hello World!');
+const dialog = document.getElementById("openDia")
+function doAns(){
+dialog.showModal()
+}
+function doCha() {
+  document.getElementById("IKI").innerHTML = "I Knew It, Gayy..."
+  dialog.close()
+  // Tab to edit
+}
